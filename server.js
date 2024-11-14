f6082a489a8eb9a75c37a219d724850982605151
DesmondObe <DesmondObe@users.noreply.github.com>
2024-11-14 22:03:50 +1000

    Setting up server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
-app.get()
-
+app.get('/', (req, res) => res.send('Hello'));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
